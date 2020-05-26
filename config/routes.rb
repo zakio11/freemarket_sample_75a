3a7fac7a1444284b885f887983e2b12ef2a6b1c9
goodriceball2100 <goodriceball2100@gmail.com>
2020-05-26 21:08:02 +0900

    modification

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
 
-  resources :items, only: [:index, :new, :create, :edit, :update]
+  resources :items, only: [:index, :new, :create, :edit, :update, :show]
   resources :users
 
 end
