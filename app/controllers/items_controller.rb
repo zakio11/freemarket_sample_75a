aee2c3c36fc6dc02ce69570ab65129a94365f3cb
yanbaru917 <ttaakkeemsss@gmail.com>
2020-05-26 11:45:45 +0900

    edit-Items-controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,27 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    if user_signend_in?
+    @item = Item.new
+    @item.image.new
+    else
+      redirect_to new_user_session_path
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name)
   end
 end
