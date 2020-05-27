0df684c1ce71a454ec101ba88861e7a1e3f71dbd
zakio <yu.m1wd11@@gmail.com>
2020-05-27 17:22:27 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    @item = "aaa"
   end
 
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
   end
 
 
