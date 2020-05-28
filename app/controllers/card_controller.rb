3caac783c9377533ddfa1fff6abe8618b5a6513f
zakio <yu.m1wd11@@gmail.com>
2020-05-28 14:15:02 +0900

    user-mypage

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -41,13 +41,6 @@ class CardController < ApplicationController
 
 
   def show
-     card = Card.where(user_id: current_user.id).first
-     if card.blank?
-       redirect_to action: "new" 
-     else
-       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-       customer = Payjp::Customer.retrieve(card.customer_id)
-       @default_card_information = customer.cards.retrieve(card.card_id)
-     end
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,5 @@ Rails.application.routes.draw do
   
   resources :items, only: [:index, :new, :create, :show, :destroy, :edit, :update]
   resources :users, only: [:show, :destroy, :logout]
-  resources :card, only: [:new, :show] do
-    collection do
-      post 'show', to: 'card#show'
-      post 'pay', to: 'card#pay'
-      post 'delete', to: 'card#delete'
-    end
-  end
+  
 end
