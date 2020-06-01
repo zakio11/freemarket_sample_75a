b19f77da4c7af70ee440fd356db8a4a2c06d0fa4
yanbaru917 <ttaakkeemsss@gmail.com>
2020-06-01 15:23:47 +0900

    new-active-hash
    アクティブハッシュの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,13 @@ class Item < ApplicationRecord
   belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :shipment_pref
+  belongs_to_active_hash :shipment_date
+  belongs_to_active_hash :shipment_fee
+  belongs_to_active_hash :item_status
+
 
   validates :name,          presence: true, length: {maximum: 40}
   validates :introduction,  presence: true, length: {maximum: 1000}
