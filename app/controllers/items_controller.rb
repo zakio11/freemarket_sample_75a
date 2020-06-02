4e517821576bd91960001ddd02d7197132ca8eee
goodriceball2100 <goodriceball2100@gmail.com>
2020-06-02 09:58:08 +0900

    画像名前価格表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,22 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.includes(:images)
   end
 
   def new
+    
   end
 
   def create
+    
   end
 
   def show
+
   end
+
+  private
+   def item_params
+    params.require(:product).permit(:name, :price, images_attributes: [:url])
+   end 
 end
